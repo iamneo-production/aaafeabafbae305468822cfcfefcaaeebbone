9cd44d1e0e75e3a3b761ae16dc400463909bf2b8
Iamneo <techadmin@iamneo.ai>
2023-09-27 07:40:52 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Declare state variables
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  // Function to handle input change
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    setGreeting(`Hello, ${name}!`); // Generate greeting message
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hey!!! Greeting</h2>
+      <form onSubmit={handleSubmit} data-testid="form">
+        <label data-testid="label">Enter your name: </label>
+        <input
+          type="text"
+          value={name}
+          onChange={handleChange}
+        />
+        <button type="submit" data-testid="buttonElement">Say Hello</button>
+      </form>
+      {greeting && <h2>{greeting}</h2>}
     </div>
   );
 }
